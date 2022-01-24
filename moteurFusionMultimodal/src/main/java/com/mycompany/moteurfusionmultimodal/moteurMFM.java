b0ba628aab617e9f14671ce8c10a10b420101b26
Caroline Senaux <caroline.senaux@alumni.enac.fr>
2022-01-24 10:24:18 +0100

    g tou fé x2

---
+++ moteurFusionMultimodal/src/main/java/com/mycompany/moteurfusionmultimodal/moteurMFM.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.moteurfusionmultimodal;
+
+import fr.dgac.ivy.Ivy;
+
+/**
+ *
+ * @author caros
+ */
+public class moteurMFM {
+    
+    private Ivy bus;
+    
+    
+
+    public moteurMFM() {
+        
+        bus = new Ivy("BUS IVY", "IVY Ready", null);
+        
+    }
+    
+    
+}
